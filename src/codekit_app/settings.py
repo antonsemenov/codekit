8fc9871f5ff38385ae1e08aec8d3af4aff62210a
antonsemenov <anton.semenov.228.1488@gmail.com>
2013-05-09 05:34:33 +0300

    static

--- src/codekit_app/settings.py
+++ src/codekit_app/settings.py
@@ -1,4 +1,5 @@
 import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Django settings for codekit_app project.
 
@@ -57,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
