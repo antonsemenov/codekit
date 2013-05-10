e991412633a95fda158bc4e8bd6d452524058d0a
antonsemenov <anton.semenov.228.1488@gmail.com>
2013-05-11 02:31:47 +0300

    View task

--- src/codekit_app/app/views.py
+++ src/codekit_app/app/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.http import Http404, HttpResponse
+
+def task_view(request, lang, task_id):
+    try:
+        lang = str(lang)
+        task_id = int(task_id)
+    except ValueError:
+        raise Http404()
+    html = '<html><body>This is %s anguage and task %s id!</body></html>' % (lang, task_id)
+    return HttpResponse(html)
