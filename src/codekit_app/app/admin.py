f9b246a16a2f7c572617403b685a8f91f4717c63
antonsemenov <anton.semenov.228.1488@gmail.com>
2013-05-11 00:20:00 +0300

    hello

--- src/codekit_app/app/admin.py
+++ src/codekit_app/app/admin.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
+
 from app.models import Task, Check, Block 
  
-admin.site.register(Task, Check, Block)
+admin.site.register(Task)
+admin.site.register(Check) 
+admin.site.register(Block)
 
