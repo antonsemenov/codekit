5c1b84818b9a0b78e4287988634b0c468b6743cd
antonsemenov <anton.semenov.228.1488@gmail.com>
2013-05-16 06:55:51 +0300

    display hames

--- src/codekit_app/app/models.py
+++ src/codekit_app/app/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Language(models.Model):
 	name  = models.CharField(max_length=40)
 	accessId = models.IntegerField()
+
 	
+    def __unicode__(self):
+        return self.name
 	
 class Task(models.Model):
     name = models.CharField(max_length=20)
@@ -11,15 +14,22 @@ class Task(models.Model):
     level = models.IntegerField(default=0)
     lang = models.ManyToManyField(Language)
 	
+	
+    def __unicode__(self):
+        return self.name
 
 class Block(models.Model):
     code = models.CharField(max_length=80)
     task =  models.ForeignKey(Task)
 
+	
+    def __unicode__(self):
+        return self.code
+        
 class Check(models.Model):
     input_value = models.CharField(max_length=40)
     output_value = models.CharField(max_length=40)
     task = models.ForeignKey(Task)
 
-
-
+    def __unicode__(self):
+        return self.input_value
